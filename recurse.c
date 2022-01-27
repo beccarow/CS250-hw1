f574e3f93e277152a716705535bb3c19db511c2a
beccarow <rr261@duke.edu>
2022-01-27 00:55:28 -0500

    fixed recurse

--- recurse.c
+++ recurse.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+#include <stdio.h>
+#include <math.h>
 
 int func(int n) {
     if (n == 0) {
         return -2;
     }
     else {
-        return 3 * n + (2 * func(n-1)) - 2;
+        return 3 * n + (2 * (func(n-1))) - 2;
     }
 }
 
-int main() {
-    int n;
+int main(int argc, char *argv[]) {
+    char *a = argv[1];
+    int n = atoi(a);
     int ans;
-
-    printf("Enter a value for n:");
-    scanf("%d", &n);
 
     ans = func(n);
 
